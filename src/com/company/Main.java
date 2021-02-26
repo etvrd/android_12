fb284d972f4aa71a0d508f99041aac00467329bf
etvrd <atomic_boy@mail.ru>
2021-02-26 13:56:52 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Main {
+
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2;
+
+    public static void main(String[] args) throws InterruptedException {
+        method1();
+        method2();
+    }
+
+    public static void method1() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = 1.0f;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void method2() {
+        float[] arr = new float[SIZE];
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = 1.0f;
+        }
+        Thread myThread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        Thread myThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a2, 0, HALF);
+        myThread1.run();
+        myThread2.run();
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+}
